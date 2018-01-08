6ab314dd6f35fea0f7965d6b52f063524f6ab24c
Baptiste TACQUET <30774289+TAC7@users.noreply.github.com>
2018-01-08 20:44:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,17 @@
 #commandes d'initialisation
 from sense_hat import SenseHat
 from time import sleep
-Import ephem 
-Import time 
-Import math
+from picamera import PiCamera
+import os
+import ephem 
+import time 
+import math
 
 sense = SenseHat()
 
 #tps
 
-#GPS
+#GPS - à tester (v1)
 Name  = "ISS (ZARYA)" 
 Line1 = "1 25544U 98067A   18008.29516252  .00001521  00000-0  30072-4 0  9994" 
 Line2 = "2 25544  51.6425  95.8146 0003157 351.7539  71.6709 15.54270870 93659" 
@@ -24,7 +26,12 @@ While True:
   Time.sleep(1) 
 
 
-#photo + heure
+#photo + heure - à tester v1  
+camera = PiCamera()
+camera.start_preview() #il manque la prise de date
+sleep(60)
+camera.stop_preview()
+
 
 #caméra infrarouge
 
