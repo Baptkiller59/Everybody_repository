017dbef0e22be6ff77a6ffbebd24fb0ac020a568
Baptiste TACQUET <30774289+TAC7@users.noreply.github.com>
2018-01-15 11:45:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ tps = #tps
 #afficher un logo pendant le démarage
 
 #liste des méthodes
+def time()
+    debut = time.time() # On attend quelques secondes avant de taper la commande suivante
+    fin = time.time()
+    tps = fin - debut # Combien de secondes entre debut et fin ?
+    
 def position() #GPS - ne marche pas (v1)
   Name  = "ISS (ZARYA)"
   Line1 = "1 25544U 98067A   18008.29516252  .00001521  00000-0  30072-4 0  9994"
@@ -34,6 +39,7 @@ def infrarouge() #photo + heure - à tester v1
   camera.stop_preview()
 
 
-while tps <= : #condition à trouver -> s'execute pendant 3heure
+while tps <= 118800: #condition à trouver -> s'execute pendant 3heures (118 800 secondes)
+  time()
   position()
   infrarouge()
