fd7940dd8be2708e5ed1121f7f80656a2a44b9ba
Baptiste TACQUET <30774289+TAC7@users.noreply.github.com>
2018-01-15 11:24:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from sense_hat import SenseHat
 from time import sleep
 from picamera import PiCamera
 import os
-import ephem 
-import time 
+import ephem
+import time
 import math
 
 sense = SenseHat()
@@ -15,30 +15,25 @@ tps = #tps
 #afficher un logo pendant le démarage
 
 #liste des méthodes
-def position() #GPS - à tester (v1)
-  Name  = "ISS (ZARYA)" 
-  Line1 = "1 25544U 98067A   18008.29516252  .00001521  00000-0  30072-4 0  9994" 
-  Line2 = "2 25544  51.6425  95.8146 0003157 351.7539  71.6709 15.54270870 93659" 
+def position() #GPS - ne marche pas (v1)
+  Name  = "ISS (ZARYA)"
+  Line1 = "1 25544U 98067A   18008.29516252  .00001521  00000-0  30072-4 0  9994"
+  Line2 = "2 25544  51.6425  95.8146 0003157 351.7539  71.6709 15.54270870 93659"
 
-  Iss = ephem.readtle(name, line1, line2) 
+  Iss = ephem.readtle(name, line1, line2)
 
   While True: #eviter une boucle infini
-    Iss.computer() 
-    Print("Lat: %s – Long: %s" % (iss.sublat, iss.sublong)) 
-    Time.sleep(1) 
+    Iss.computer()
+    Print("Lat: %s – Long: %s" % (iss.sublat, iss.sublong))
+    Time.sleep(1)
 
-def photo() #photo + heure - à tester v1  
+def infrarouge() #photo + heure - à tester v1
   camera = PiCamera()
   camera.start_preview() #il manque la prise de date
   sleep(60)
   camera.stop_preview()
 
-def infrarouge() #caméra infrarouge
-
 
 while tps <= : #condition à trouver -> s'execute pendant 3heure
   position()
-  photo()
-  infrarouge()  
-  
-  
+  infrarouge()
